56578ed52f2732ffa01de276af39c42c2cfb984e
Bernard1010 <bernahall1010@gmail.com>
2016-05-23 20:36:38 -0700

    Modulo top del control del teclado
    Creacion del modulo que controla el teclado y que genera las salidas en
    forma de pulsos para su utilizacion como botones del proyecto

--- Repositorio_3/Top.v
+++ Repositorio_3/Top.v
@@ -21,31 +21,31 @@
 module TOP(clk, 
            ps2c, 
            ps2d, 
-           rx_en, 
+            
            code);
 
     input clk;
     input ps2c;
     input ps2d;
-    input rx_en;
+    
    output [3:0] code;
    reg [23:0]cont=24'b0;
 	reg reset= 1'b0;
    
    wire [7:0] XLXN_8;
 	
-	
+	  
    
    ps2_rx  XLXI_1 (.clk(clk), 
                   .ps2c(ps2c), 
                   .ps2d(ps2d), 
                   .reset(reset), 
-                  .rx_en(rx_en), 
+                  .rx_en(1'b1), 
                   .dout(XLXN_8[7:0]), 
                   .rx_done_tick());
    deco  XLXI_2 (.clk(clk), 	
                 .code_i(XLXN_8[7:0]),  
-                .code_o(code[3:0]));
+                 .code_o(code[3:0]));
 					 
 					 
 always@(posedge clk)
